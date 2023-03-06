e0dd38eb3d25eb71b4bc9473adfe725e3d9447d0
Achyut Behera <serverless.think@gmail.com>
2023-03-06 18:01:50 +0530

    Serverless example eventbridge

--- serverless-framework/nodejs/f7-sls-eventbridge-lambda/CreateStudent.js
+++ serverless-framework/nodejs/f7-sls-eventbridge-lambda/CreateStudent.js
@@ -1,11 +1,11 @@
 module.exports.createStudent = async (event) => {
   console.log(JSON.stringify(event));
-  const student = JSON.parse(event.Records[0].Sns.Message);
+  const student = event.detail;
   console.log(
     `Student Details ${student.id} - ${student.name} - ${student.rollNo}`
   );
   return {
     statusCode: 201,
-    body: JSON.stringify(`Student ${student.id} created`),
+    body: JSON.stringify(`Student ${student.id} received`),
   };
 };
